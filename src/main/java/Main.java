945b59ddc057ecc423d6b47920f4417787bfb903
Ezgi Efe <ezgiefee@gmail.com>
2022-09-11 21:24:26 +0300

    Second Problem

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args){
-        FirstProblem firstProblem = new FirstProblem();
-        firstProblem.result();
+
+        SecondProblem secondProblem = new SecondProblem();
+        secondProblem.result();
+
     }
 }
---
+++ src/main/java/SecondProblem.java
@@ -0,0 +1,33 @@
+//By considering the terms in the Fibonacci sequence
+// whose values do not exceed four million, find the
+// sum of the even-valued terms.
+public class SecondProblem {
+
+    public int result(){
+        int sum = 0;
+        int count = 1;
+        int result = 0;
+
+       while(count>0){
+           result = fibonacci(count);
+           if(result > 4000000){
+               break;
+           }
+           if(result % 2 == 0L){
+               System.out.println("Result : " + result);
+               sum += result;
+           }
+           count++;
+        }
+        return sum;
+    }
+
+    public int fibonacci(int n){
+        if(n == 0 || n == 1){
+            return n;
+        } else {
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
+    }
+
+}

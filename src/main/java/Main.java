2d24323a03f2ab846cc5e70ded72d6a4b8c2b33d
Ezgi Efe <ezgiefee@gmail.com>
2022-09-19 21:48:02 +0300

    problem 8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        PS7 ps7 = new PS7();
-        System.out.println("The 10001st prime number is " + ps7.getPrimeNumbers());
+        PS8 ps8 = new PS8();
+        ps8.adjacents();
     }
 }

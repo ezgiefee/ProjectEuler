b8c08ee8805658dc0adf4d449309a490de341ad9
Ezgi Efe <ezgiefee@gmail.com>
2022-09-20 19:53:09 +0300

    problem 10

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        PS9 ps9 = new PS9();
-        ps9.findPythagoreanTriplet();
+
+        System.out.println(PS10.getPrimeNumbersSum());
     }
 }
---
+++ src/main/java/PS10.java
@@ -0,0 +1,21 @@
+//The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+//Find the sum of all the primes below two million.
+
+public class PS10 {
+    public static Long getPrimeNumbersSum(){
+        Long result = 2L;
+        int count = 0;
+        for (Long i = 3L;i <= 2000000L; i+=2){
+            for(Long j = 2L; j<= i ; j++){
+                if( i % j == 0){
+                    count++;
+                }
+            }
+            if(count == 1){
+                result += i;
+            }
+            count = 0;
+        }
+        return result;
+    }
+}

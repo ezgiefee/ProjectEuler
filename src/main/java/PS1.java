769428f2dbbdfa2d90370f3d02a468aee5bf19d3
Ezgi Efe <ezgiefee@gmail.com>
2022-10-26 00:11:28 +0300

    1. problem

--- src/main/java/PS1.java
+++ src/main/java/PS1.java
@@ -1,7 +1,10 @@
+//Find the sum of all the multiples of 3 or 5 below 1000
 public class PS1 {
-    //Find the sum of all the multiples of 3 or 5 below 1000
-    private int sum =0;
-    public int result(){
+    public static void main(String[] args) {
+        result();
+    }
+    private static int sum =0;
+    public static int result(){
         for(int i=0; i<1000; i++){
             if(i%3 == 0 || i%5 == 0){
                 sum += i;

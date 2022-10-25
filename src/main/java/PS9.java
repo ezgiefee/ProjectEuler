4f84a98ce1eace4ff3eed22b23893b298ce01a9c
Ezgi Efe <ezgiefee@gmail.com>
2022-10-26 00:24:56 +0300

    9. problem

--- src/main/java/PS9.java
+++ src/main/java/PS9.java
@@ -3,6 +3,9 @@
 //There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 //Find the product abc.
 public class PS9 {
+    public static void main(String[] args) {
+        findPythagoreanTriplet();
+    }
     public static void findPythagoreanTriplet(){
         for(int a=1;a<=1000;a++){
             for(int b= 1; b<=1000-a; b++){

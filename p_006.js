e2d8a488d60408fbb48519bb7feeeb2449dd5d6a
Ezgi Efe <ezgiefee@gmail.com>
2023-05-20 01:12:57 +0300

    Sixth Euler Problem Solution

--- p_006.js
+++ p_006.js
@@ -5,7 +5,6 @@ for (let i = 1; i <= 100; i++){
   sumOfSquares += Math.pow(i, 2);
   squareOfSum += i;
 }
-
 squareOfSum = Math.pow(squareOfSum, 2);
 
 console.log("The difference between two numbers is ", squareOfSum - sumOfSquares);

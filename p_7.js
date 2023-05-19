dad837214cb1450bde5870ff52a8472428097456
Ezgi Efe <ezgiefee@gmail.com>
2023-05-19 14:52:56 +0300

    Seventh Problem Solution

---
+++ p_7.js
@@ -0,0 +1,27 @@
+var number = 2;
+var primeCount = 1;
+
+
+while (primeCount < 10001) {
+  number++;
+  if (prime(number)) {
+    primeCount++;
+  }
+}
+
+console.log("The 10001th prime number is: ",number);
+
+function prime(number) {
+  let temp = 0;
+  for (let i = 2; i <= number/2; i++){
+    if (number % i == 0) {
+      temp++;
+      break;
+    }
+  }
+  if (temp == 0 && number != 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
